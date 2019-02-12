4dcac676ccfe84c1df68d03feebc7ec1b7bdee57
John Poulson <Jdpoulson@gmail.com>
2019-02-12 14:56:24 -0800

    created a new fortune teller website

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+var unlucky = 0
+var lucky = 0
+$(document).ready(function(){
+  $("form#fortune_teller").submit(function(event){
+    event.preventDefault();
+    $("input:checkbox[name=unlucky]:checked").each(function(){
+      unlucky++
+    });
+    $("input:checkbox[name=lucky]:checked").each(function(){
+      lucky++
+    });
+    $("#fortune_teller").hide();
+    if((unlucky >= 4) && (lucky <= 3)) {
+      $("#unlucky-fortune").show();
+      $("#lucky-fortune").hide();
+    };
+    if((lucky >= 4) && (unlucky <= 3)) {
+      $("#lucky-fortune").show();
+      $("#unlucky-fortune").hide();
+    };
+  });
+})
